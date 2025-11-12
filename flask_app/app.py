0b8bff88e40de78777dce3f44b1f9f0466292168
hadiji <hadijisami45@gmail.com>
2025-11-12 11:03:08 +0100

    auto commit

--- flask_app/app.py
+++ flask_app/app.py
@@ -166,4 +166,4 @@ def api_colis():
         return jsonify([]), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
